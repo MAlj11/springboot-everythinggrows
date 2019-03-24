3fd60abf1d59e6af33bae436da5852aeace930bd
MA <malijun1114@outlook.com>
2019-03-24 20:07:14 +0800

    boot

---
+++ egboot-user/src/main/java/cn/everythinggrows/boot/egboot/user/MyException.java
@@ -0,0 +1,31 @@
+package cn.everythinggrows.boot.egboot.user;
+
+public class MyException extends Throwable{
+    private static final long serialVersionUID = 1L;
+
+    private String code;
+    private String msg;
+
+    public MyException() {}
+
+    public MyException(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
